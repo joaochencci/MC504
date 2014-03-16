d6b27b3fdf8e8e3b25dd63a10da635e5e4ea5da2
unknown <guimba.costa@gmail.com>
2014-03-16 01:08:15 -0300

    modelo

--- main.c
+++ main.c
@@ -1,40 +1,171 @@
-/*
- * Projeto 01 - MC504 - 1s2014
- *
- * André Seiji Tamanaha			116134
- * Guilherme Costa Zanelato		119494
- * João Victor Chencci Marques	119637
- *
- * */
-
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-#define N_THR 10
+#define HORIZONTAL_CHECK 1
+#define VERTICAL_CHECK 2
+#define SUBMATIX_CHECK 3
+#define NONNUMBER_FOUND 4
+
+int flag = 0;               /* 1 - Error, 0 - Success */
+
+int printNotValid(char *array, int type, int errorLocation);
+int check(char *array);
+void printUsage();
+
+int main(int argc, char *argv[])
+{
+    FILE *data;
+    if (argc != 2)
+        {
+            printUsage();
+            exit(1);
+        }
+    else
+        data = fopen(argv[1], "r");
+
+    /* Reading the Matrix from file */
+    int c;
+    int i = 0, j = 0;
+
+    char matrix[9][9];
+    while ((c = getc(data)) != EOF)
+        {
+            if (c == ' ')
+                continue;
+            else if (c == '\n')
+                {
+                    i = 0;
+                    j++;
+                }
+            else
+                matrix[j][i++] = c;
+        }
 
 
-void* f_thread(void *v) {
-  int thr_id;
-  sleep(1);
-  thr_id = (int) v;
-  printf("Thread %d.\n", thr_id);
-  printf("João!");
-  return NULL;
+    /* Printing the matrix */
+    printf("\n -- sudoku -- \n");
+    for (i = 0; i < 9; i++)
+        {
+            for (j = 0; j < 9; j++)
+                printf("%c ", matrix[i][j]);
+            printf("\n");
+        }
+
+
+    /* If the sudoku contains other than numbers, exit, printing error */
+    printf("\n");
+    for (i = 0; i < 9; i++)
+        {
+            for (j = 0; j < 9; j++)
+                {
+                    if ( (matrix[i][j] < '0') || (matrix[i][j] > '9'))
+                        {
+                            printf(" Error (%d,%d): Sudoku can contain only numbers \n",
+                                   i+1, j+1);
+                        }
+                }
+        }
+
+    char buffer[9];
+
+    /* Horizontal check */
+    for (i = 0; i < 9; i++)
+        {
+            for (j = 0; j < 9; j++)
+                buffer[j] = matrix[i][j];
+
+            if (check(buffer) != 0)
+                printNotValid(buffer, HORIZONTAL_CHECK, i);
+        }
+
+    /* Vertical check */
+    for (j = 0; j < 9; j++)
+        {
+            for (i = 0; i < 9; i++)
+                {
+                    buffer[i] = matrix[i][j];
+                }
+            if (check(buffer) != 0)
+                printNotValid(buffer, VERTICAL_CHECK, j);
+        }
+
+    /* Submatrix check */
+    int count = 0;
+    int icount = 0;
+    int jcount = 0;
+    int matcount = 0;
+
+    for (i = 0; i <= 6; i+= 3)
+        {
+            for (j = 0; j <= 6; j += 3)
+                {
+                    for (icount = 0, count = 0; icount < 3; icount++)
+                        {
+                            for (jcount = 0; jcount < 3; jcount++)
+                                {
+                                    buffer[count++] = matrix[i+icount][j+jcount];
+                                }
+
+                        }
+
+                    ++matcount;
+                    if (check(buffer) != 0)
+                        printNotValid(buffer, SUBMATIX_CHECK, matcount);
+                }
+        }
+
+    if (flag == 0)
+        printf("\n The Sudoku is correct :)");
+
+    printf("\n");
+    return 0;
 }
 
+int check(char *array)
+{
+    int i, count;
+    for (i = 0; i < 9; i++)
+        {
+            count = i;
+            while (count < 9)
+                {
+                    if (array[i] == array[++count])
+                        return 1;
+                }
+        }
+    return 0;
+}
 
+int printNotValid(char *array, int type, int errorLocation)
+{
+    int i;
+    flag = 1;
+    if (type == HORIZONTAL_CHECK)
+        {
+            printf("\n Found error at row %d: \n\t", ++errorLocation);
+            for (i = 0; i < 9; i++)
+                printf("%c ", array[i]);
+        }
+    else if (type == VERTICAL_CHECK)
+        {
+            printf("\n Found error at column %d: \n", ++errorLocation);
+            for (i = 0; i < 9; i++)
+                printf("\n\t %c ", array[i]);
+        }
+    else if (type == SUBMATIX_CHECK)
+        {
+            printf("\n Found error at Submatrix %d: \n", errorLocation);
+            for (i = 0; i < 9; i++)
+                {
+                    printf("%c ", array[i]);
+                    if ((i == 2) || (i == 5))
+                        printf("\n");
+                }
 
-int main() {
-  pthread_t thr[N_THR];
-  int i;
+        }
+}
 
-  for (i = 0; i < N_THR; i++)
-    pthread_create(&thr[i], NULL, f_thread, (void*) i);
-
-  for (i = 0; i < N_THR; i++)
-    pthread_join(thr[i], NULL);
-
-  return 0;
+void printUsage()
+{
+    printf("Usage: sudokucheck <data_file>\n");
 }
